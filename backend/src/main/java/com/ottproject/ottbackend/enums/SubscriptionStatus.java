4ce4f228453ec049112e7960a40aeb43aff2b1a6
KimGyuhyun <kgh980605@gmail.com>
2025-08-14 15:54:30 +0900

    feat: Added skip type and subscription status enums

---
+++ backend/src/main/java/com/ottproject/ottbackend/enums/SubscriptionStatus.java
@@ -0,0 +1,15 @@
+package com.ottproject.ottbackend.enums;
+
+/**
+ * 구독 상태
+ * - ACTIVE: 활성
+ * - CANCELED: 해지
+ * - EXPIRED: 만료
+ */
+public enum SubscriptionStatus {
+    ACTIVE,
+    CANCELED,
+    EXPIRED
+}
+
+
