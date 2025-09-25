dea74e898ccd225e95cb8f60fa7fd57d323c0264
KimGyuhyun <kgh980605@gmail.com>
2025-09-25 18:32:30 +0900

    style: adjust header spacing and icon alignment

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -284,7 +284,7 @@ export default function Header() {
 
                       {/* 메뉴 항목들 */}
                       <div className={styles.menuItems}>
-                        {/* 라프텔 멤버십 */}
+                        {/* 라퓨타 멤버십 */}
                         <Link
                           href={getMembershipLink()}
                           onClick={() => setIsProfileOpen(false)}
@@ -293,7 +293,7 @@ export default function Header() {
                           <svg className={styles.menuIcon} fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" />
                           </svg>
-                          <span>라프텔 멤버십</span>
+                          <span>라퓨타 멤버십</span>
                         </Link>
                         
                         {/* 이용내역 */}
