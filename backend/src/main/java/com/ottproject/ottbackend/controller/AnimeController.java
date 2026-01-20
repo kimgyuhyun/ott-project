a961868149c8ef3dc356c55484b02fd0ac43edef
KimGyuhyun <kgh980605@gmail.com>
2026-01-20 18:44:52 +0900

    docs: Add comments by interpreting the code

--- backend/src/main/java/com/ottproject/ottbackend/controller/AnimeController.java
+++ backend/src/main/java/com/ottproject/ottbackend/controller/AnimeController.java
@@ -128,7 +128,6 @@ public class AnimeController {
     @ApiResponse(responseCode = "200", description = "조회 성공")
     @GetMapping("/popular")
     public List<AnimeListDto> getPopular() {
-        // 평점 기준으로 정렬하여 상위 10개 반환
         return queryService.list(
                 null, // status
                 null, // genreIds
